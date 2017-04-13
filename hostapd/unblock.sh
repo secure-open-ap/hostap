e022de946e7779c379aafd3f9e09948613db50e4
Seokseong Jeon <songsong@monet.postech.ac.kr>
2017-04-13 21:15:31 +0900

    AP: Edit unblock.sh for different nmcli version

--- hostapd/unblock.sh
+++ hostapd/unblock.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/sudo /bin/bash
+
 if [ -z "$1" ]; then
   echo "Interface must be specified. Exit."
   exit 1
 fi
-nmcli nm wifi off
+# check nmcli version
+# old: `nmcli nm`
+# new: `nmcli r`
+nmcli nm > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+  nmcli nm wifi off
+else
+  nmcli r wifi off
+fi
 rfkill unblock wlan
 ifconfig $1 up
 
